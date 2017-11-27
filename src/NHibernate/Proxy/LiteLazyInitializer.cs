ff117d23395aae27aa40f09c997841ce179b0435
Alexander Zaytsev <hazzik@gmail.com>
2017-11-27 14:09:35 +1300

    Mark LiteLazyInitializer as Serializable

--- src/NHibernate/Proxy/LiteLazyInitializer.cs
+++ src/NHibernate/Proxy/LiteLazyInitializer.cs
@@ -1,8 +1,10 @@
+using System;
 using NHibernate.Engine;
 using NHibernate.Proxy.DynamicProxy;
 
 namespace NHibernate.Proxy
 {
+	[Serializable]
 	class LiteLazyInitializer : AbstractLazyInitializer, DynamicProxy.IInterceptor
 	{
 		internal LiteLazyInitializer(string entityName, object id, ISessionImplementor session, System.Type persistentClass) 
